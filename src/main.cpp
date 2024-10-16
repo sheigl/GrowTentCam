094513abd9c90cf0f8e1513683e2ecf8655f147a
Steven Heigl <sheigl@gmail.com>
2024-10-15 21:54:09 -0400

    working

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,84 @@
 #include <WiFi.h>
 #include "esp_timer.h"
 #include "img_converters.h"
-
+// #include "FS.h"     // SD Card ESP32
+// #include "SD_MMC.h" // SD Card ESP32
 #include "fb_gfx.h"
 #include "soc/soc.h"          //disable brownout problems
 #include "soc/rtc_cntl_reg.h" //disable brownout problems
 #include <WiFi.h>
-#include "mqttwrapper.h"
-#include <EspCam.h>
 #include <settings.h>
-//#include <ArduinoOTA.h>
-
-MqttWrapper pubSubClient;
-EspCam cam;
-Settings settings;
+#include <PubSubClient.h>
+#include <driver/rtc_io.h>
 
 unsigned long lastPhotoTaken = millis();
+PubSubClient pubSubClient;
+WiFiClient espClient;
+
+typedef struct
+{
+  uint8_t *buffer = nullptr;
+  size_t length = 0;
+} Photo, *pPhoto;
+
+void connectWifi();
+void disconnectWifi();
+bool connectPubSub();
+void publishPhoto(Photo *photo);
+void log(const String message);
+Photo *takePhoto();
+void setupCamera();
+void setupPubSub();
+
+void setup()
+{
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable brownout detector
+  setupCamera();
+  setupPubSub();
+
+  Serial.begin(9600);
+  Serial.setDebugOutput(false);
+}
+
+void loop()
+{
+  unsigned long now = millis();
+
+  if (now - lastPhotoTaken > DELAY_TIME)
+  {
+    Photo *photo = takePhoto();
+    connectWifi();
+
+    if (photo->length > 0)
+    {
+      log("Sending photo...");
+      publishPhoto(photo);
+    }
+    else
+    {
+      log("Unable to take photo...");
+    }
+
+    pubSubClient.disconnect();
+    disconnectWifi();
+
+    lastPhotoTaken = now;
+  }
+
+  pubSubClient.loop();
+  delay(1);
+}
+
+void setupPubSub()
+{
+  pubSubClient.setClient(espClient);
+  pubSubClient.setServer(HOST, 1883);
+}
 
 void connectWifi()
 {
   // Wi-Fi connection
-  WiFi.begin(settings.wifiSsid, settings.wifiPassword);
+  WiFi.begin(WIFI_SSID, WIFI_PWD);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -35,81 +93,152 @@ void disconnectWifi()
   WiFi.disconnect();
 }
 
-void setup()
+bool connectPubSub()
 {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable brownout detector
+  if (!WiFi.isConnected())
+    return false;
 
-  Serial.begin(9600);
-  Serial.setDebugOutput(false);
-
-  cam.Setup();
-
- /*  ArduinoOTA
-      .onStart([]()
-               {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH) {
-        type = "sketch";
-      } else {  // U_SPIFFS
-        type = "filesystem";
-      }
-
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type); })
-      .onEnd([]()
-             { Serial.println("\nEnd"); })
-      .onProgress([](unsigned int progress, unsigned int total)
-                  { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
-      .onError([](ota_error_t error)
-               {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) {
-        Serial.println("Auth Failed");
-      } else if (error == OTA_BEGIN_ERROR) {
-        Serial.println("Begin Failed");
-      } else if (error == OTA_CONNECT_ERROR) {
-        Serial.println("Connect Failed");
-      } else if (error == OTA_RECEIVE_ERROR) {
-        Serial.println("Receive Failed");
-      } else if (error == OTA_END_ERROR) {
-        Serial.println("End Failed");
-      } });
-
-  ArduinoOTA.begin(); */
-}
-
-void loop()
-{
-  unsigned long now = millis();
-
-  if (now - lastPhotoTaken > settings.delayTime)
+  while (!pubSubClient.connected())
   {
-    Photo photo = cam.CaptureFrame();
+    Serial.println("Connecting to message broker...");
+    pubSubClient.connect(HOST_NAME, MQ_USR, MQ_PWD);
 
-    connectWifi();
-
-    if (pubSubClient.Connect())
-    {
-      if (photo.length > 0)
-      {
-        pubSubClient.Publish("log", "Sending photo...");
-        pubSubClient.Publish("snapshot", photo.length, photo.buffer);
-      }
-      else
-      {
-        pubSubClient.Publish("log", "Unable to take photo...");
-        // ESP.restart();
-      }
-
-      pubSubClient.Disconnect();
-    }
-
-    disconnectWifi();
-
-    lastPhotoTaken = now;
+    delay(250);
   }
 
-  pubSubClient.Loop();
-  //ArduinoOTA.handle();
-  delay(1);
+  return pubSubClient.connected();
+}
+
+char *createTopic(String topic)
+{
+  String strTopicBase(MQ_TOPIC);
+  String fullTopic = strTopicBase + "/" + topic;
+  char *topicBuffer = (char *)malloc(fullTopic.length() + sizeof(char *));
+
+  fullTopic.toCharArray(topicBuffer, fullTopic.length() + sizeof(char *));
+
+  return topicBuffer;
+}
+
+void publishPhoto(Photo *photo)
+{
+  if (connectPubSub())
+  {
+    char *topicBuffer = createTopic("snapshot");
+
+    log("Sending payload to: " + String(topicBuffer));
+    pubSubClient.beginPublish(topicBuffer, photo->length, false);
+
+    size_t res;
+    uint32_t offset = 0;
+    uint32_t to_write = photo->length;
+    uint32_t buf_len;
+
+    do
+    {
+      buf_len = to_write;
+      if (buf_len > 64000)
+        buf_len = 64000;
+
+      log("Sending bytes " + String(offset + buf_len) + "/" + String(photo->length));
+      res = pubSubClient.write(photo->buffer + offset, buf_len);
+
+      offset += buf_len;
+      to_write -= buf_len;
+
+    } while (res == buf_len && to_write > 0);
+
+    bool published = pubSubClient.endPublish();
+
+    if (!published)
+      log("Unable to send photo through mq...");
+
+    free(topicBuffer);
+  }
+}
+
+void log(const String message)
+{
+  Serial.println(message);
+  char *topicBuffer = createTopic("log");
+  char *payloadBuffer = (char *)malloc(message.length() + sizeof(char *));
+  message.toCharArray(payloadBuffer, message.length() + sizeof(char *));
+
+/*   if (connectPubSub())
+  {
+    pubSubClient.publish(topicBuffer, payloadBuffer, message.length());
+  } */
+
+  free(topicBuffer);
+  free(payloadBuffer);
+}
+
+void setupCamera()
+{
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+
+  if (psramFound())
+  {
+    config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  }
+  else
+  {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // Init Camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+Photo *takePhoto()
+{
+  log("Capturing frame...");
+
+  camera_fb_t *fb = esp_camera_fb_get();
+  log("Got frame");
+
+  uint8_t *buffer;
+  size_t length;
+
+  buffer = fb->buf;
+  length = fb->len;
+
+  log("Resetting frame buffer...");
+  esp_camera_fb_return(fb);
+  fb = NULL;
+
+  pPhoto photo = (pPhoto)malloc(sizeof(photo));
+  photo->buffer = buffer;
+  photo->length = length;
+
+  return photo;
 }
--- src/settings.h
+++ src/settings.h
@@ -1,22 +1,29 @@
-#ifndef __SETTINGS_H__
-#define __SETTINGS_H__
+#define WIFI_SSID "Internetz"
+#define WIFI_PWD "javajuice"
+#define HOST "192.168.4.202"
+#define MQ_USR "mosquitto"
+#define MQ_PWD "86LSwHF0JSAf"
+#define MQ_TOPIC "growtentcam"
+#define DELAY_TIME 10000
+#define HOST_NAME "GrowTentCam"
+#define EEPROM_SIZE 1
 
-class Settings
-{
-private:
-    /* data */
-public:
-    unsigned long delayTime;
-    const char *wifiSsid = nullptr;
-    const char *wifiPassword = nullptr;
-    const char *host = nullptr;
-    const char *mqUser = nullptr;
-    const char *mqPassword = nullptr;
-    const char *mqTopicBase = nullptr;
-    const char *hostName = nullptr;
+#define PART_BOUNDARY "123456789000000000000987654321"
 
-    Settings();
-    ~Settings();
-};
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
 
-#endif // __SETTINGS_H__
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
